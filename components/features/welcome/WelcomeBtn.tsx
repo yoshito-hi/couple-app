a68f163eb8828e17dc6b36befb5f9da785db850e
yoshito-hi <yoshito.140821@gmail.com>
2026-04-29 16:29:42 +0900

    welcomeBtn 作成

--- components/features/welcome/WelcomeBtn.tsx
+++ components/features/welcome/WelcomeBtn.tsx
@@ -1,16 +1,16 @@
 import { AppCard } from "@/components/base/AppCard"
 import { AppIcon } from "@/components/base/AppIcon"
 import { User, ArrowRight } from "lucide-react"
-import { TypographyP } from "@/components/base/typography/typography"
+import { TypographyH1 } from "@/components/base/typography/typography"
 
 export const WelcomeBtn = () => {
   return (
     <div>
       <AppCard className="flex flex-row items-center justify-center p-4 cursor-pointer transition-all duration-200 hover:-translate-y-0.5">
         <AppIcon icon={User} />
-        <TypographyP className="text-lg font-zen-maru">
+        <TypographyH1 className="text-lg">
           ＊＊としてログイン
-        </TypographyP>
+        </TypographyH1>
         <AppIcon icon={ArrowRight} variant="ghost" />
       </AppCard>
     </div>
